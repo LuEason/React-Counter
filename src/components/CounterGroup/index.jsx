aedf4fdeb5f30fff1683d6cc3c1de44e3611a2d9
Eason <chenzscd@163.com>
2020-08-03 16:25:24 +0800

    [React][Eason] feature: add value attribute to input

--- src/components/CounterGroup/index.jsx
+++ src/components/CounterGroup/index.jsx
@@ -16,7 +16,7 @@ class CounterGroup extends React.Component {
   render() {
     return (
       <div>
-        number of counters: <input onChange={this.inputChange}></input>
+        number of counters: <input value={this.state.numberOfCounts} onChange={this.inputChange}></input>
         {
           new Array(this.state.numberOfCounts).fill(0).map((value, index) => {
             return <Count key={index} />
