e9ab5da58462dbbdccbb4bd69751bd19ce79e71e
Eason <chenzscd@163.com>
2020-08-03 15:09:38 +0800

    [React][Eason] feature: add Counter and CounterGroup components

---
+++ src/components/CounterGroup/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Count from '../Counter';
+
+class CounterGroup extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                {new Array(6).fill(0).map((value, index) => {
+                    return <Count key={index}/>
+                })}
+            </div>
+        )
+    }
+}
+
+export default CounterGroup;
